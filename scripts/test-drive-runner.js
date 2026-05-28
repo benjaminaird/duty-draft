35874841bb4304f734d94536fbd4b78b35bee31d
Benjamin Aird <aird.drumcorps@gmail.com>
2026-05-27 21:58:21 -0400

    Add runner imports

--- scripts/test-drive-runner.js
+++ scripts/test-drive-runner.js
@@ -1,1 +1,13 @@
-module.exports = { ok: true };
+const axios = require('axios');
+const TEST_MARINES = require('./data/test-marines.json');
+const {
+  getWeekendDates,
+  weekendQuota,
+  selectWeekendMarines,
+  buildDraftOrder,
+  getAllDates,
+  isDateValid,
+  isWkDate
+} = require('./test-drive-helpers');
+
+module.exports = { TEST_MARINES };
