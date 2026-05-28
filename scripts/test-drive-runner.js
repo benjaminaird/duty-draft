c4c7115fde0d7bc8cac381d10adb8c9ca393696b
Benjamin Aird <aird.drumcorps@gmail.com>
2026-05-27 22:01:19 -0400

    Add weekend setup helper

--- scripts/test-drive-runner.js
+++ scripts/test-drive-runner.js
@@ -18,3 +18,28 @@ async function seedMonth(baseUrl) {
 }
 
 module.exports = { TEST_MARINES, seedMonth };
+
+async function applyWeekendSetup(baseUrl, state) {
+  const weekendDates = getWeekendDates(state);
+  const weekendAssignments = selectWeekendMarines(TEST_MARINES, weekendDates.length, state.history || {});
+
+  state.wkAssignees = weekendAssignments;
+  state.wkAssigneeIds = [
+    ...weekendAssignments.junior.map(m => m.id),
+    ...weekendAssignments.ssgt.map(m => m.id),
+    ...weekendAssignments.gysgt.map(m => m.id)
+  ];
+  state.weekendDates = weekendDates;
+  state.phase = 'wkpreview';
+
+  await axios.post(`${baseUrl}/api/state`, state);
+
+  return {
+    state: (await axios.get(`${baseUrl}/api/state`)).data,
+    weekendDates,
+    weekendAssignments,
+    quota: weekendQuota(weekendDates.length)
+  };
+}
+
+module.exports = { TEST_MARINES, seedMonth, applyWeekendSetup };
