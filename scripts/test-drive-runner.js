a513f634fac356250a0c9a9ec51dd5ebc75cbb30
Benjamin Aird <aird.drumcorps@gmail.com>
2026-05-27 22:00:04 -0400

    Add month seeding helper

--- scripts/test-drive-runner.js
+++ scripts/test-drive-runner.js
@@ -11,3 +11,10 @@ const {
 } = require('./test-drive-helpers');
 
 module.exports = { TEST_MARINES };
+
+async function seedMonth(baseUrl) {
+  await axios.post(`${baseUrl}/api/state`, { marines: TEST_MARINES, turnMins: 1 });
+  return (await axios.get(`${baseUrl}/api/state`)).data;
+}
+
+module.exports = { TEST_MARINES, seedMonth };
